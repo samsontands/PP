c5e981b53a47446b6cd27309b6d229a5fc106210
Samson Tan <139669515+samsontands@users.noreply.github.com>
2024-07-03 17:26:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,73 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
+from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
-from st_aggrid import AgGrid
-import ydata_profiling
-from ydata_profiling import ProfileReport
-from PIL import Image
 
-st.set_page_config(layout="wide")
-st.sidebar.title("Welcome to EDAverse")
-with st.sidebar.expander("About the App💻"):
-    st.write("""You can use the EDAverse to quickly generate a comprehensive data profiling and EDA report
-     without even writing a single line code.📊📊
-    """)
-st.title("EDAverse📊💹")
-st.write("Explore. Discover. Analyze.")
-img="https://i.postimg.cc/sXJ8Rcrp/SQC-AND-OR-UNIT-1.png"
-st.image(img,use_column_width=True)
-upload_file=st.file_uploader("Upload Your CSV File:",type=["csv"])
-if upload_file is not None:
-    df=pd.read_csv(upload_file)
-    option1=st.sidebar.radio("What do want to include in the report?",["All variable","A subset of Variable"])
-    if option1=="All variable":
-        df=df
-    elif option1=="A subset of Variable":
-        var_list=list(df.columns)
-        option3=st.sidebar.multiselect("Select the variables(s) you want to include in the report",var_list)
-        df=df[option3]
-    option2=st.sidebar.selectbox("Chose Minimal Mode or Complete Mode",("Minimal Mode","Complete Mode"))
-    if option2=="Complete Mode":
-        mode="complete"
-        st.sidebar.warning("Complete Mode takes time,PLEASE BE PATIENT!")
-    elif option2=="Minimal Mode":
-        mode="minimal"
-    grid_response=AgGrid(df,editable=True,height=300,width="100%")
-    updated=grid_response["data"]
-    df1=pd.DataFrame(updated)
 
-    #Now let's build the report
-    if st.button("Generate Button"):
-        if mode=="complete":
-            profile=ProfileReport(df,progress_bar=True)
-            st.write("⬇️Report is given below⬇️")
-            st_profile_report(profile)
-            profile.to_file("your_report.html")
-            
-        elif mode=="minimal":
-            profile=ProfileReport(df1,minimal=True,progress_bar=True)
-            st.write("⬇️Report is given below⬇️")
-            st_profile_report(profile)
-        if "profile" in locals():
-            st.download_button("Download Report", data=profile.to_html(), file_name="your_report.html")
+def main():
+    PAGES = {
+        "EDA": render_eda,
+        "About": render_about,
+    }
+
+    st.title("Streamlit")
+    st.sidebar.header("Options")
+    page = st.sidebar.selectbox(
+        "Upload your data to create an Exploratory Data Analysis",
+        options=list(PAGES.keys())
+    )
+    PAGES[page]()
+
+
+def render_eda():
+    st.title("Create a Complete Report of your data.")
+    st.subheader("Exploratory Data Analysis using pandas profiling.")
+    st.write("""All you need to do is upload a dataset and get a quick
+            sense of your data.""")
+
+    data_size = st.selectbox(
+        'What kind of dataset size are you uploading?',
+        ('Small', 'Large')
+    )
+
+    st.write('You selected data size option: ', data_size)
+
+    data = st.file_uploader("Upload Dataset", type=["csv", "txt"])
+
+    if data is not None:
+        if data_size == 'Small':
+            df = pd.read_csv(data)
+            pr = ProfileReport(df, explorative=True)
+            st.title("Pandas Profiling Report in Streamlit")
+            st.dataframe(df)
+            st_profile_report(pr)
+            # pr.to_file("Output.html")
+            st.write("Your report is ready!")
+        else:
+            df = pd.read_csv(data)
+            pr = ProfileReport(df,  minimal=True)
+            st.title("Pandas Profiling Report in Streamlit")
+            st.dataframe(df)
+            st_profile_report(pr)
+            # pr.to_file("Output.html")
+            st.write("Your report is ready!")
+
+
+def render_about():
+    st.write(
+        """An App facilitating exploratory data analysis by using
+            pandas profiling for streamlit."""
+    )
+    st.markdown(
+        """Thank you [okld](
+                https://github.com/okld/streamlit-pandas-profiling) for
+                creating the pandas profiling component for streamlit.
+                Check out the [pandas-profiling](
+                https://pandas-profiling.github.io/pandas-profiling/docs/master/rtd/index.html)
+                main page for more ways to configure your exploratory analysis.
+            """
+    )
+
+
+if __name__ == "__main__":
+    main()
